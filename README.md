e24ec932f081064d52df39ee937dd8d6162b9454
Peter Kanev <pip3r4o@gmail.com>
2017-08-30 16:25:04 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ns-dev-days git init
---
+++ server/models/session.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// mock DB
+const sessionsDay1 = require("../db/sessions-data-day1.json") || [];
+const sessionsDay2 = require("../db/sessions-data-day2.json") || [];
+const sessions = sessionsDay1.concat(sessionsDay2);
+
+const SESSIONS_DAY2_STARTING_ID = 8;
+
+module.exports = (Session) => {
+    Session.get = (sessionId, cb) => {
+        console.log(arguments);
+        console.log(sessionId);
+        respond(() => {
+            sessionId = parseInt(sessionId);
+            if (isNumber(sessionId)) {
+                let session;
+                // slight optimization to avoid having to search through the concatenated array
+                if (sessionId < SESSIONS_DAY2_STARTING_ID) {
+                    // search in day1 sessions
+                    session = sessionsDay1.find((element) => {
+                        return element.id === sessionId;
+                    });
+                } else {
+                    // search in day2 sessions
+                    session = sessionsDay2.find((element) => {
+                        return element.id === sessionId;
+                    });
+                }
+
+                if (session) {
+                    cb(null, JSON.stringify(session));
+                } else {
+                    cb(null, "Session with id " + sessionId + " not found.");
+                }
+            } else {
+                cb(null, "Session id must be a valid number.");
+            }
+        })
+    }
+
+    Session.getAll = (cb) => {
+        console.log("Called getALL");
+        respond(() => {
+            // join the sessions from both days
+            cb(null, JSON.stringify(sessions));
+        })
+    }
+};
+
+function respond(response) {
+    process.nextTick(response);
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
