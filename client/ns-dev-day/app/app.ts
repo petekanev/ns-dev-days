db08f451fc61c44ebcaf4ae0d7528a767ed82c4c
Peter Kanev <pip3r4o@gmail.com>
2017-08-31 18:54:12 +0300

    update fetching of sessions and apply inline styles (wip)

--- client/ns-dev-day/app/app.ts
+++ client/ns-dev-day/app/app.ts
@@ -6,7 +6,8 @@ purpose of the file is to pass control to the app’s first module.
 import * as app from "application";
 import "./bundle-config";
 
-app.start({ moduleName: "home/home-page" });
+// app.start({ moduleName: "home/home-page" });
+app.start({ moduleName: "sessions/sessions-page" });
 
 /*
 Do not place any code after the application has been started as it will not
--- client/ns-dev-day/app/sessions/sessions-view-model.ts
+++ client/ns-dev-day/app/sessions/sessions-view-model.ts
@@ -3,6 +3,8 @@ import { ObservableArray } from "data/observable-array";
 import * as http from "http";
 
 const WEB_SERVER_DOMAIN = "http://10.0.2.2:3000/api/sessions"
+const monthsArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 
 export class SessionsViewModel extends Observable {
     public sessionsList: ObservableArray<any>;
@@ -16,26 +18,28 @@ export class SessionsViewModel extends Observable {
             console.log(res);
             const jsonRes = JSON.parse(res.sessions)
             if (jsonRes.length > 0) {
-                jsonRes.forEach((element : any) => {
-                    that.sessionsList.push(new SessionEntry(element.name, element.time.toLocaleString(), element.length, element.speakers));
+                jsonRes.forEach((element: any) => {
+                    that.sessionsList.push(new SessionEntry(element.name, new Date(element.time), element.length, element.speakers));
                 });
             }
         }, (err) => {
             alert("Couldn't access web server: " + JSON.stringify(err));
-        });
+        }).catch((rej) => { console.log("Rejected: " + rej) });
     }
 }
 
 class SessionEntry {
     public timeString: string;
+    public dayString: string;
     public name: string;
     public speakers: string[];
-    public duration: number;
+    public duration: string;
 
-    constructor(name, time, duration, speakers) {
+    constructor(name, time: Date, duration, speakers) {
         this.name = name;
-        this.timeString = time;
-        this.duration = duration;
+        this.timeString = time.getHours() + ":" + (time.getMinutes() === 0 ? "00" : time.getMinutes());
+        this.dayString = time.getUTCDate() + " " + monthsArr[time.getUTCMonth()];
+        this.duration = duration + " mins";
         this.speakers = speakers;
     }
 }
