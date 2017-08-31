177158641906c54ca6b4ae052df0423db58c267d
Peter Kanev <pip3r4o@gmail.com>
2017-08-31 15:07:33 +0300

    fetching doesnt parse properly

--- client/ns-dev-day/app/sessions/sessions-view-model.ts
+++ client/ns-dev-day/app/sessions/sessions-view-model.ts
@@ -1,7 +1,39 @@
 import { Observable } from "data/observable";
+import { ObservableArray } from "data/observable-array";
+import * as http from "http";
+
+const WEB_SERVER_DOMAIN = "http://10.0.2.2:3000/api/sessions"
 
 export class SessionsViewModel extends Observable {
+    public sessionsList: ObservableArray<any>;
+
     constructor() {
         super();
+
+        http.getJSON(WEB_SERVER_DOMAIN + "/getAll").then((res: any) => {
+            console.log(res);
+            const jsonRes = JSON.parse(res)
+            if (jsonRes) {
+                res.forEach((element : any) => {
+                    this.sessionsList.push(new SessionEntry(element.name, element.time.toLocaleString(), element.length, element.speakers));
+                });
+            }
+        }, (err) => {
+            alert("Couldn't access web server: " + JSON.stringify(err));
+        });
+    }
+}
+
+class SessionEntry {
+    public timeString: string;
+    public name: string;
+    public speakers: string[];
+    public duration: number;
+
+    constructor(name, time, duration, speakers) {
+        this.name = name;
+        this.timeString = time;
+        this.duration = duration;
+        this.speakers = speakers;
     }
 }
