f6aa3ae3967e43c7a979555d386f2a050bfd0aba
Peter Kanev <pip3r4o@gmail.com>
2017-09-08 16:24:11 +0300

    broke it.

--- client/ns-dev-day/app/home/home-view-model.ts
+++ client/ns-dev-day/app/home/home-view-model.ts
@@ -3,42 +3,45 @@ import { Label } from "ui/label";
 
 export class HomeViewModel extends Observable {
     public countdown: string = "N/A";
-    public announcement: string =  "SEPTEMBER 18-19, 2017 / NEW YORK CITY\n" +
-                "NATIVESCRIPT DEVELOPER DAY 2017\n" +
-                "JAVASCRIPT. MOBILE. NATIVE.";
+    public announcement: string = "SEPTEMBER 23-24, 2018 / IT'S A FAKE\n" +
+    "NATIVESCRIPT DEVELOPER DAY 2018\n" +
+    "JAVASCRIPT. MOBILE. NATIVE.";
     public countdownClass: string = "no-countdown";
 
     private countDownComponent: android.os.CountDownTimer;
     constructor() {
         super();
-        const toDate = new Date("2017-09-18T08:30:00-04:00");
-        const endDate = new Date("2017-09-19T17:00:00-04:00");
+        const toDate = new Date("2018-09-23T08:30:00-04:00");
+        const endDate = new Date("2018-09-24T17:00:00-04:00");
         const currentDate = new Date();
         const difference = toDate.getTime() - currentDate.getTime();
-        const isOver = (currentDate.getTime() - endDate.getTime()) > 0;
-
-        if (isOver) {
-            this.set("countdown", "DeveloperDay is over :-(");
-            this.set("announcement", "We'll see you again next year!");
-
-            return;
-        }
-
-        if (difference < 0) {
-            this.set("countdown", "DeveloperDay is here!");
-            
-            return;
-        }
-
-        this.set("countdownClass", "countdown-widget");
 
         const updateCountDown = (countdown) => {
-            this.set("countdown", millisecondsToReadableTime(countdown));
+            this.countdown = millisecondsToReadableTime(countdown);
         }
 
         if (global.android) {
-            ensureCountDownTimer();
-            this.countDownComponent = new CountDownTimerClass(difference, 1000, updateCountDown).start();
+            class CountDownTimer extends android.os.CountDownTimer {
+                private updateCountDownCB;
+
+                constructor(millisInFuture: number, interval: number, updateCountDownCB) {
+                    super(millisInFuture, interval);
+
+                    this.updateCountDownCB = updateCountDownCB;
+
+                    return global.__native(this);
+                }
+
+                public onTick(millisUntilFinished) {
+                    this.updateCountDownCB(millisUntilFinished);
+                }
+
+                public onFinish() {
+                    this.updateCountDownCB("NativeScript Dev Days is here!");
+                }
+            }
+
+            this.countDownComponent = new CountDownTimer(difference, 1000, updateCountDown).start();
         }
     }
 }
@@ -53,31 +56,6 @@ function millisecondsToReadableTime(milliseconds: number) {
     readableHours = readableHours > 9 ? readableHours : "0" + readableHours;
     let days: number | string = Math.floor(hours / 24);
     days = days > 1 ? `${days} days` : "1 day";
-    
+
     return `${days}, ${readableHours}:${minutes}:${seconds}`;
-}
-
-let CountDownTimerClass;
-function ensureCountDownTimer() {
-    if (!CountDownTimerClass) {
-        CountDownTimerClass = class CountDownTimer extends android.os.CountDownTimer {
-            private updateCountDownCB;
-
-            constructor(millisInFuture: number, interval: number, updateCountDownCB) {
-                super(millisInFuture, interval);
-
-                this.updateCountDownCB = updateCountDownCB;
-
-                return global.__native(this);
-            }
-
-            public onTick(millisUntilFinished) {
-                this.updateCountDownCB(millisUntilFinished);
-            }
-
-            public onFinish() {
-                this.updateCountDownCB("NativeScript Dev Days is here!");
-            }
-        }
-    }
 }
--- client/ns-dev-day/app/tweets/tweets-view-model.ts
+++ client/ns-dev-day/app/tweets/tweets-view-model.ts
@@ -3,38 +3,48 @@ import { ObservableArray } from "data/observable-array";
 import { request, HttpRequestOptions } from "http";
 
 export class TweetsViewModel extends Observable {
-   static NSTweets: ObservableArray<Tweet> = new ObservableArray();
+    static NSTweets: ObservableArray<Tweet> = new ObservableArray();
+    static mockTweets: Tweet[] = [
+        {
+            createdAt: "Thu Sep 07 17:38:12 +0000 2017",
+            text: "What a #BeautifulWorld would it be for us developers if we didn't have to test our apps in shitty phones #FirstWorldProblems #Nativescript \ud83d\ude12",
+            userName: "Alvaro Nicoli",
+            screenName: "@xmr_nkr",
+            profileImageUrl: "http:\/\/pbs.twimg.com\/profile_images\/895006836247711745\/4TMle4xQ_normal.jpg",
+        },
+        {
+            createdAt: "Thu Sep 07 17:38:12 +0000 2017",
+            text: "What a #BeautifulWorld.",
+            userName: "Alvaro Nicoli",
+            screenName: "@xmr_nkr",
+            profileImageUrl: "http:\/\/pbs.twimg.com\/profile_images\/895006836247711745\/4TMle4xQ_normal.jpg",
+        }
+    ]
 
     constructor() {
         super();
-        
-        if (TweetsViewModel.NSTweets.length < 1) {
-            const requestOptions: HttpRequestOptions = {
-                url: "https://api.twitter.com/1.1/search/tweets.json?q=%23NativeScript&result_type=recent",
-                headers: {
-                    Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAAAqV2AAAAAAAAW%2BRfD5YPhcJmnEQErGpBw2gDGA%3DxOTFG633CY24wqx1Z1I7zdaJDh0He5UNF0n6D0RvcM3T1nKmYw"
-                },
-                method: "GET"
-            };
 
-            request(requestOptions).then((res) => {
-                if (res.statusCode >= 200 && res.statusCode < 300) {
-                    let jsonRes = res.content.toJSON();
-                    let resTweets = jsonRes.statuses;
-                    resTweets.forEach(element => {
-                        const tweet = new Tweet(element.createdAt, element.text, element.user.name, element.user.screen_name, element.user.profile_image_url);
-                        TweetsViewModel.NSTweets.push(tweet);
-                        debugger;
-                    });
-                } else {
-                    alert("Failed fetching Twitter statuses. Code: " + res.statusCode);
-                }
-            }, (rej) => {
-                alert("Failed to make request to Twitter API. " + rej);
-            }).catch((err) => {
-                alert("Failed to make request to Twitter API. " + err);
-            });
-        }
+        const requestOptions: HttpRequestOptions = {
+            url: "https://api.twitter.com/1.1/search/tweets.json?q=%23NativeScript&result_type=recent",
+            headers: {
+                Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAAAqV2AAAAAAAAW%2BRfD5YPhcJmnEQErGpBw2gDGA%3DxOTFG633CY24wqx1Z1I7zdaJDh0He5UNF0n6D0RvcM3T1nKmYw"
+            },
+            method: "GET"
+        };
+
+        TweetsViewModel.mockTweets.forEach(tweet => TweetsViewModel.NSTweets.push(tweet));
+
+        // request(requestOptions).then((res) => {
+        //     if (res.statusCode >= 200 && res.statusCode < 400) {
+
+        //     } else {
+        //         alert("Failed fetching Twitter statuses. Code: " + res.statusCode);
+        //     }
+        // }, (rej) => {
+        //     alert("Failed to make request to Twitter API. " + rej);
+        // }).catch((err) => {
+        //     alert("Failed to make request to Twitter API. " + err);
+        // });
     }
 
     get tweets() {
